90b189b5344d5d12271d6b0655699d3cedb029c9
Kfir Lankry <lankrykfir@gmail.com>
2022-11-09 22:42:04 +0200

    .

--- ShortURL-Client/src/components/Navbar.jsx
+++ ShortURL-Client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ function Navbar() {
     <div>
       <nav className="navBar navbar-expand-lg bg-dark navbar-dark ">
         <p className="title navbar-brand text-center pt-3" href="#">
+          <img style={{ width: "2.2rem" }} src="logo.png" alt="Logo" />{" "}
           ShortURLs
         </p>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
